7c663aaf2ce5f70a6285b2c4da05999785c5fd65
GeissonChagas <geisson.excel@gmail.com>
2023-01-25 22:36:08 -0300

    add: html/css

---
+++ scripts.js
@@ -0,0 +1,66 @@
+var products = [
+    {
+        "photo": "img/nike-air-force.png",
+        "name": "Nike Air Force azul",
+        "price": 219.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-air-force-laranja.pnj",
+        "name": "Nike Air Force laranja",
+        "price": 229.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-air-max-cinza.png",
+        "name": "Nike Air Max Cinza",
+        "price": 279.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-air-max-coral.png",
+        "name": "Nike Air Max Coral",
+        "price": 159.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-air-max-preto.png",
+        "name": "Nike Air max Preto",
+        "price": 349.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-air-max-preto-e-branco.png",
+        "name": "Nike Air Max Preto e Branco",
+        "price": 259.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-air-max-rosa.png",
+        "name": "Nike Air Max Rosa",
+        "price": 329.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-club-max.png",
+        "name": "Nike Club Max",
+        "price": 199.00,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nike-jordan.png",
+        "name": "Nike Jordan",
+        "price": 189.00,
+        "active": false,
+        "quantity": 1
+    }
+];
+ 
